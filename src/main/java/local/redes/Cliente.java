2e9387c6aa13a20e2279547d2e2687db1cfe3f9c
KaueSanchesG <kauesanches123@gmail.com>
2025-09-10 13:29:00 -0300

    feat: adding server&client, needing implementation on isCPFvalid

--- src/main/java/local/redes/Cliente.java
+++ src/main/java/local/redes/Cliente.java
@@ -1,8 +1,28 @@
 package local.redes;
 
+import java.io.*;
+import java.net.Socket;
+
+// @author Kauê S. Gonçalves
+
 public class Cliente {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        Socket socket = new Socket("127.0.0.1", 50000);
+        DataOutputStream saida = new DataOutputStream(socket.getOutputStream());
+        DataInputStream entrada = new DataInputStream(socket.getInputStream());
+
+        BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Digite o cpf");
+        var cpf = teclado.readLine();
+
+        saida.writeUTF(cpf);
+        System.out.println(entrada.readUTF());
+
+        saida.close();
+        entrada.close();
+        socket.close();
     }
 }
--- src/main/java/local/redes/Servidor.java
+++ src/main/java/local/redes/Servidor.java
@@ -1,8 +1,43 @@
 package local.redes;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+// @author Kauê S. Gonçalves
+
 public class Servidor {
 
-    public static void main(String[] args) {
+    private static boolean isCPFValid(String cpf){
+        return false;
+    }
 
+    public static void main(String[] args) throws IOException {
+        ServerSocket servidor = new ServerSocket(50000);
+        System.out.println("Porta " + servidor.getLocalPort() + " aberta, iniciando servidor");
+
+        Socket socket = servidor.accept();
+
+        DataInputStream entrada = new DataInputStream(socket.getInputStream());
+        DataOutputStream saida = new DataOutputStream(socket.getOutputStream());
+
+        var cpf = entrada.readUTF();
+
+        System.out.println("Entrada: " + cpf);
+
+        var resposta = isCPFValid(cpf) ? "Este CPF é válido" : "Este CPF é inválido.";
+
+        System.out.println(resposta);
+
+        saida.writeUTF(resposta);
+
+        System.out.println("Fechando conexão " + servidor.getLocalPort());
+
+        entrada.close();
+        saida.close();
+        socket.close();
     }
 }
