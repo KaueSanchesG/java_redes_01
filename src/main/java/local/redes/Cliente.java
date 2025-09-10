0880abcba121dd45ca7c86895c44b2f8acbbd02c
KaueSanchesG <kauesanches123@gmail.com>
2025-09-10 12:47:45 -0300

    first commit

---
+++ src/main/java/local/redes/Cliente.java
@@ -0,0 +1,8 @@
+package local.redes;
+
+public class Cliente {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/local/redes/Servidor.java
@@ -0,0 +1,8 @@
+package local.redes;
+
+public class Servidor {
+
+    public static void main(String[] args) {
+
+    }
+}
